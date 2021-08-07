da4ef27da4dc5e861b72b9b354917e01e47fbe6c
swatser <swathikk101@gmail.com>
2021-08-07 12:40:45 -0400

    Cleaned up

---
+++ js/index.js
@@ -0,0 +1,9 @@
+console.log("good job Swathi");
+
+$( "td" ).hover(
+    function() {
+      $( this ).addClass( "hover" );
+    }, function() {
+      $( this ).removeClass( "hover" );
+    }
+  );
